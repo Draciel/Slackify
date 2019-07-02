96f13df8b3bd4dd805e984cc2faa284b01a00f52
Jakub Tyrka <jakub.t.tyrka@gmail.com>
2019-07-02 15:42:37 +0200

    Replace @Controller with @RestController in App

--- src/main/java/pl/draciel/slackify/SlackifyApp.java
+++ src/main/java/pl/draciel/slackify/SlackifyApp.java
@@ -2,11 +2,10 @@ package pl.draciel.slackify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class SlackifyApp {
 
@@ -15,7 +14,6 @@ public class SlackifyApp {
     }
 
     @GetMapping({"/", "home"})
-    @ResponseBody
     String startPage() {
         return "Welcome!";
     }
